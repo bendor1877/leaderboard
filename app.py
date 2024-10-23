805b6a7d8b79f645b40ee600135e46e7a974cb3a
bendor1877 <bendor1877@proton.me>
2024-10-23 19:02:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.metrics import roc_auc_score
+import sqlite3
 
 
 # ページのレイアウトを設定
@@ -13,13 +14,34 @@ st.set_page_config(
 # タイトルの設定
 st.title("リーダーボード")
 
-df_ref = pd.read_csv("正解_bin.csv")
-st.write(df_ref)
 
 # サイドバーにアップロードファイルのウィジェットを表示
 st.sidebar.markdown("TEST")
-uploaded_file = st.sidebar.file_uploader("計算結果をCSVファイルでアップロードしてください", type='csv')
+uploaded_file = st.sidebar.file_uploader("計算結果のCSVファイルをアップロードしてください", type='csv')
 if uploaded_file is not None:
     df_res = pd.read_csv(uploaded_file)
-    auc = roc_auc_score(df_ref["値"], df_res["値"])
-    st.write(auc)
+
+with st.sidebar.form(clear_on_submit=False):
+    name = st.text_input("ニックネームを入力してください(他の参加者にも表示されます)")
+    submitted = st.form_submit_button("回答を送信")
+
+if submitted:
+    if uploaded_file is not None:
+        # DBファイル読み込み
+        conn = sqlite3.connect("lb.db")
+        c = conn.cursor()
+
+        # 正解データを読み込み
+        df_ref = pd.read_csv("正解_bin.csv")
+
+        # アップロードファイル読み込み
+        df_res = pd.read_csv(uploaded_file)
+        
+        # AUC計算
+        auc = roc_auc_score(df_ref["値"], df_res["値"])
+
+        print(f"{name}: {auc}")
+
+    else:
+        st.alert("計算結果のファイルがアップロードされていません。") 
+        st.stop()
