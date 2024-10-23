a1399ebb0e188c4de12129ff7fb53b4caa9f40c7
bendor1877 <bendor1877@proton.me>
2024-10-23 19:13:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -18,10 +18,8 @@ st.title("リーダーボード")
 # サイドバーにアップロードファイルのウィジェットを表示
 st.sidebar.markdown("TEST")
 uploaded_file = st.sidebar.file_uploader("計算結果のCSVファイルをアップロードしてください", type='csv')
-if uploaded_file is not None:
-    df_res = pd.read_csv(uploaded_file)
 
-with st.sidebar.form(key="from", clear_on_submit=False):
+with st.sidebar.form(key="form", clear_on_submit=False):
     name = st.text_input("ニックネームを入力してください(他の参加者にも表示されます)")
     submitted = st.form_submit_button("回答を送信")
 
@@ -35,7 +33,7 @@ if submitted:
         df_ref = pd.read_csv("正解_bin.csv")
 
         # アップロードファイル読み込み
-        df_res = pd.read_csv(uploaded_file)
+        df_res = pd.read_csv(uploaded_file.seek(0))
         
         # AUC計算
         auc = roc_auc_score(df_ref["値"], df_res["値"])
