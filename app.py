f15ed4f69be6f8f8af3a89d97d4f7485b6754f3b
bendor1877 <bendor1877@proton.me>
2024-10-23 18:01:25 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.metrics import roc_auc_score
 
 
 # ページのレイアウトを設定
@@ -12,12 +13,13 @@ st.set_page_config(
 # タイトルの設定
 st.title("リーダーボード")
 
-df_ref = pd.read_csv("正解.csv")
+df_ref = pd.read_csv("正解_bin.csv")
 st.write(df_ref)
 
 # サイドバーにアップロードファイルのウィジェットを表示
 st.sidebar.markdown("TEST")
 uploaded_file = st.sidebar.file_uploader("計算結果をCSVファイルでアップロードしてください", type='csv')
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    st.write(data)
+    df_res = pd.read_csv(uploaded_file)
+    auc = roc_auc_score(df_ref["正解"], df_res["値"])
+    st.write(auc)
